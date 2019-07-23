d798a116e29065a042fbd9197b4cf8f87ddf7dad
naliniganesan <51950030+naliniganesan@users.noreply.github.com>
2019-07-23 15:27:31 +0530

    Create 72.py

---
+++ 72.py
@@ -0,0 +1,7 @@
+n21=input()
+for i in n21:
+   if(i=="a" or i=="e" or i=="i" or i=="o" or i=="u" or i="A" or i="E" or i=="I" i=="O" or i=="U"):
+         print("yes")
+         break
+ else:
+      print("no")
